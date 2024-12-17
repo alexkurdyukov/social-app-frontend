e4cf07a43b44594658cabaf08b8e3bcd0b3d1a70
KurdyukovAlA <leha.kurdyukov.99@mail.ru>
2024-12-17 13:51:12 +0300

    Добавление rtk query конфига для отправки запросов

---
+++ src/app/services/commentApi/commentsApi.ts
@@ -0,0 +1,27 @@
+import { api } from '../api';
+import { Comment } from '../../types/types';
+
+export const commentsApi = api.injectEndpoints({
+    endpoints: builder => ({
+        createComment: builder.mutation<Comment, Partial<Comment>>({
+            query: newComment => ({
+                url: `/comments`,
+                method: 'POST',
+                body: newComment,
+            }),
+        }),
+        deleteComment: builder.mutation<void, string>({
+            query: commentId => ({
+                url: `/comments/${commentId}`,
+                method: 'DELETE',
+            }),
+        }),
+    }),
+});
+
+export const { useCreateCommentMutation, useDeleteCommentMutation } =
+    commentsApi;
+
+export const {
+    endpoints: { createComment, deleteComment },
+} = commentsApi;
