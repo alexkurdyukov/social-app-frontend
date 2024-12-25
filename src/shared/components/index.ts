e05d95e22ccef7d009648a8a4facc169bcee6469
KurdyukovAlA <leha.kurdyukov.99@mail.ru>
2024-12-25 11:36:21 +0300

    Добавление алиасов

---
+++ src/shared/components/index.ts
@@ -0,0 +1,8 @@
+import { Button } from './Button/Button';
+import { Container } from './Container/Container';
+import { Header } from './Header/Header';
+import { Layout } from './Layout/Layout';
+import { Navbar } from './Navbar/Navbar';
+import { NavButton } from './NavButton/NavButton';
+
+export { Button, Container, Header, Layout, Navbar, NavButton };
